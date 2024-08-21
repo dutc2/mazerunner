e1c76546a9dfbe8651f789f3dd08f2945a8ff708
James Powell <james@dutc.io>
2024-08-20 22:28:33 -0400

    make modules easier to import (removing module-level runtime code)

--- examples/solver.py
+++ examples/solver.py
@@ -1,6 +1,8 @@
-from mazerunner import Request, Response, connection
 from time import sleep
+from logging import getLogger, basicConfig, INFO, DEBUG
 from random import Random
+
+from mazerunner import Request, Response, connection, run_agent, parser
 
 def turn_until_free(tick, *, random_state=None):
     rnd = random_state if random_state is not None else Random()
@@ -27,20 +29,28 @@ def simplesolver(tick, *, random_state=None):
         yield from move_until_stopped(tick=tick)
         sleep(tick)
 
-with connection(host=args.host, port=args.port) as send:
-    resp = send(req := Request.Test())
-    logger.info('Request → Response: %16r → %r', req, resp)
+if __name__ == '__main__':
+    args = parser.parse_args()
+    logger = getLogger(__name__)
+    basicConfig(level={0: INFO, 1: DEBUG}.get(args.verbose, INFO))
 
-    ci = simplesolver(tick=1, random_state=Random(0))
+    agent_kwargs= {'host': args.host, 'port': args.port, 'maze': args.maze, 'seed': args.seed, 'errors': args.errors, 'tick': args.tick}
+    run_agent(**agent_kwargs)
 
-    resp = None
-    while True:
-        try:
-            req = ci.send(resp)
-        except StopIteration:
-            break
-        resp = send(req)
+    with connection(host=args.host, port=args.port) as send:
+        resp = send(req := Request.Test())
         logger.info('Request → Response: %16r → %r', req, resp)
 
-    resp = send(req := Request.ExitSensor())
-    logger.info('Request → Response: %16r → %r', req, resp)
+        ci = simplesolver(tick=1, random_state=Random(0))
+
+        resp = None
+        while True:
+            try:
+                req = ci.send(resp)
+            except StopIteration:
+                break
+            resp = send(req)
+            logger.info('Request → Response: %16r → %r', req, resp)
+
+        resp = send(req := Request.ExitSensor())
+        logger.info('Request → Response: %16r → %r', req, resp)
